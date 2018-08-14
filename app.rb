cf21625393bab8109c47e0e98859b40b8b1b69d6
Jiaming Lou <stylerlight101@gmail.com>
2018-08-14 19:06:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Mimi."
+  end
+  
+  get '/hometown' do
+    "My hometown is Western Springs."
+  end
+  
 end
